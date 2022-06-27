67f5fa3e3e4db10eb8e04403932dd322e43446d0
xCode-Breakerx <59203346+xCode-Breakerx@users.noreply.github.com>
2022-06-27 12:23:06 +0200

    Added persistent theme

--- src/app/header/navbar/navbar.component.ts
+++ src/app/header/navbar/navbar.component.ts
@@ -1,20 +1,32 @@
-import { Component, OnInit }        from '@angular/core';
-import { MaterialIcon360Component } from "../../animations/material/icons/materialicon360/material-icon360.component";
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MaterialIcon360Component }                    from "../../animations/material/icons/materialicon360/material-icon360.component";
 
 @Component({
              selector   : 'app-navbar',
              templateUrl: './navbar.component.html',
              styleUrls  : ['./navbar.component.scss']
            })
-export class NavbarComponent implements OnInit
+export class NavbarComponent implements OnInit, AfterViewInit
 {
+  @ViewChild('switch') mode: MaterialIcon360Component = null!
 
   constructor()
   {
+
   }
 
   ngOnInit(): void
   {
+
+  }
+
+  ngAfterViewInit(): void
+  {
+    if( localStorage.getItem("theme") == "dark" )
+    {
+      this.mode.animate = true;
+      document.getElementById("__root__")?.classList.add("theme-alternate")
+    }
   }
 
   SwitchMode(mode: MaterialIcon360Component)
@@ -22,9 +34,11 @@ export class NavbarComponent implements OnInit
     mode.toggle();
     if( mode.animate )
     {
+      localStorage.setItem("theme", "dark")
       document.getElementById("__root__")?.classList.add("theme-alternate")
     } else
     {
+      localStorage.setItem("theme", "light")
       document.getElementById("__root__")?.classList.remove("theme-alternate")
     }
   }
