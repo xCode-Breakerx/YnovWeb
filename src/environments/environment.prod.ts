632fb1bca0f4201ccfcdd1ca8b42ecbe57003bc4
xGODSx <79153942+xGODSx@users.noreply.github.com>
2022-07-09 19:47:27 +0200

    Updated prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production : true,
+  cat_api_key: "1e9157f9-f4c8-4ed4-9f27-d3d5bd6b1815",
+  pwd        : "%coffee%#69",
+  user       : "codebreakerumbra"
 };
